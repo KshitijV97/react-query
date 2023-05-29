4c4e84b222e670826f27f98caa788755ed6f0017
Kshitij Vengurlekar <writetokshitij@gmail.com>
2023-05-29 09:47:10 +0530

    Success Error callbacks

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,25 +1,33 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
+const fetchSuperheroes = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
+
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data, isError, error, refetch } = useQuery(
+  const onSuccess = (data) =>
+    console.log("Perform side errect after data fetching", data);
+
+  const onError = (error) =>
+    console.log("Perform side errect after encountering error", error);
+
+  const { isLoading, data, isError, error } = useQuery(
     ["super-heroes"],
-    () => {
-      return axios.get("http://localhost:4000/superheroes");
-    },
+    fetchSuperheroes,
     {
-      enabled: false,
+      onSuccess: onSuccess,
+      onError: onError,
     }
   );
 
-  // if (isLoading) return <h2>Loading...</h2>;
+  if (isLoading) return <h2>Loading...</h2>;
 
   if (isError) return <h2>{error.msg}</h2>;
 
   return (
     <div>
       <h2>RQ Super heroes page</h2>
-      <button onClick={refetch}>Fetch heroes</button>
       {data?.data.map((hero) => {
         return <div key={hero.name}>{hero.name}</div>;
       })}
