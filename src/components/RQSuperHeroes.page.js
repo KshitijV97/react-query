ccff4646f8ce8886003894c47e50452bb2574115
Kshitij Vengurlekar <writetokshitij@gmail.com>
2023-05-29 09:22:15 +0530

    Fetch on Click

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -2,17 +2,24 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data, isError, error } = useQuery(["super-heroes"], () => {
-    return axios.get("http://localhost:4000/superheroes");
-  });
+  const { isLoading, data, isError, error, refetch } = useQuery(
+    ["super-heroes"],
+    () => {
+      return axios.get("http://localhost:4000/superheroes");
+    },
+    {
+      enabled: false,
+    }
+  );
 
-  if (isLoading) return <h2>Loading...</h2>;
+  // if (isLoading) return <h2>Loading...</h2>;
 
   if (isError) return <h2>{error.msg}</h2>;
 
   return (
     <div>
       <h2>RQ Super heroes page</h2>
+      <button onClick={refetch}>Fetch heroes</button>
       {data?.data.map((hero) => {
         return <div key={hero.name}>{hero.name}</div>;
       })}
